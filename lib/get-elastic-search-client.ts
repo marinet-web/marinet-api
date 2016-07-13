f425bdf4ab8be945e91a351b025b494d6bb8b9a6
Leonardo Cardoso <kibiluzbad@gmail.com>
2016-07-13 19:02:12 -0300

    Update composer file

--- lib/get-elastic-search-client.ts
+++ lib/get-elastic-search-client.ts
@@ -1,7 +1,7 @@
 import { Client } from 'elasticsearch';
 
 export const client = new Client({
-    host: process.env.ELASTIC_URL || 'localhost:9200',
+    host: process.env.SEARCHBOX_URL || 'localhost:9200',
     log: process.env.ELASTIC_LOG_LEVEL || 'trace'
 });
 
