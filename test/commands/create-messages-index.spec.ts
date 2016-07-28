dc49cc5550073b9848be0c6a41549bb4592f9b9c
Leonardo Cardoso <kibiluzbad@gmail.com>
2016-07-28 16:30:27 -0300

    Create message index test

--- test/commands/create-messages-index.spec.ts
+++ test/commands/create-messages-index.spec.ts
@@ -2,67 +2,85 @@
 import 'reflect-metadata';
 import * as TypeMoq from "typemoq";
 import { Promise } from 'es6-promise';
-import { Db, Server, Collection, InsertOneWriteOpResult, ObjectID } from "mongodb";
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
-import { Kernel } from 'inversify';
+import { Client, IndexDocumentParams, Indices } from 'elasticsearch';
 
-import { CreateApplication, CreateUser, GetMongoDB } from '../../lib/commands';
-import { Config, Application, User } from '../../lib/models';
-import { TYPES } from '../../lib/types';
-
+import { CreateMessagesIndex } from '../../lib/commands';
+import {  } from '../../lib/models';
 
 const expect = chai.expect;
 chai.use(chaiAsPromised);
 
-
 describe('Create messages index command', () => {
 
-    let createUser: TypeMoq.Mock<CreateUser>;
-    let getMongoDb: TypeMoq.Mock<GetMongoDB>;
-    let server: TypeMoq.Mock<Server>;
-    let db: TypeMoq.Mock<Db>;
-
-    beforeEach(() => {
-        createUser = TypeMoq.Mock.ofType(CreateUser);
-        getMongoDb = TypeMoq.Mock.ofType(GetMongoDB);
-        server = TypeMoq.Mock.ofType(Server, TypeMoq.MockBehavior.Loose, 'localhost', 27017);
-        db = TypeMoq.Mock.ofType(Db, TypeMoq.MockBehavior.Loose, 'dbname', server.object);
-    });
+    let client: Client;
 
     describe('When calling exec()', () => {
-        let app: Application;
-        let config = <Config>{ appSecret: "secret" };
 
         beforeEach(() => {
 
-            let collection = <Collection>{
-                insert: (param) => {
-                    return Promise.resolve(<InsertOneWriteOpResult>{
-                        ops: [param]
-                    });
+            client = <Client>{
+                indices: <Indices>{
+                    create: (params) => {
+                        return {
+                            then: (result, error) => {
+                                result({
+                                    "acknowledged": true
+                                });
+                            }
+                        };
+                    }
                 }
             };
-
-            let user = <User>{
-                _id: new ObjectID(),
-                name: 'user_appName',
-                email: 'user@appName',
-                password: 'password',
-                permissions: ['logger']
-            };
-
-
-            db.setup(c => c.collection(TypeMoq.It.isAnyString())).returns(() => collection);
-
-            getMongoDb.setup(c => c.exec())
-                .returns(() => Promise.resolve(db.object));
-
-            createUser.setup(c => c.exec()).returns(() => Promise.resolve(user));
         });
 
         it('should return true', () => {
-            //expect.fail();
+            let command = new CreateMessagesIndex(client);
+            return expect(command.exec()).to.eventually.have.property('acknowledged').true;
         });
+
+        describe('If index already exists', () => {
+
+            beforeEach(() => {
+
+                client = <Client>{
+                    indices: <Indices>{
+                        create: (params) => {
+                            return {
+                                then: (result, error) => {
+                                    error({
+                                        "error": {
+                                            "root_cause": [
+                                                {
+                                                    "type": "index_already_exists_exception",
+                                                    "reason": "already exists",
+                                                    "index": "newindex"
+                                                }
+                                            ],
+                                            "type": "index_already_exists_exception",
+                                            "reason": "already exists",
+                                            "index": "newindex"
+                                        },
+                                        "status": 400
+                                    });
+                                }
+                            };
+                        }
+                    }
+                };
+            });
+
+            it('should throw', (done) => {
+                let command = new CreateMessagesIndex(client);
+                command.exec().then((result) => {
+                    done('Should not be OK');
+                }, err =>{
+                    expect(err).to.have.property('status').eql(400);
+                    done();
+                })
+            });
+        })
     });
+
 });
